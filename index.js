a501ed991cf661c3c1455db7c607579b935e6770
TammyXie4677 <2474677@johnabbottcollege.net>
2024-10-25 21:33:01 -0400

    Connected to JawsDB MYSQL

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the JawsDB MySQL database!');
+});
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
