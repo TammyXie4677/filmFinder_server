80166d89ed65c6bbcddd7ce293335f415db5a10c
TammyXie4677 <2474677@johnabbottcollege.net>
2024-10-28 19:56:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the JawsDB MySQL database!');
-});
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { connectDB } = require('./sequelize');
+const authRoutes = require('./routes/auth');
+const User = require('./models/User');
 
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
 app.use(express.static(path.join(__dirname, '/public')));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB().then(async () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
 
