7c7f188d719b28d00b3052faf7eba29541073818
qwqtw <qiutianw@gmail.com>
2024-10-28 22:29:45 -0400

    Movies

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+const moviesRouter = require('./routes/movies');
+app.use('/movies', moviesRouter);
+
 connectDB().then(async () => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
